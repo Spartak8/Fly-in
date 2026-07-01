f28cd623f4eb36d8fb4a62ae277b1745c07823d1
Spartak <77225767+Spartak8@users.noreply.github.com>
2026-07-01 11:41:56 +0400

    Add files via upload

---
+++ models.py
@@ -0,0 +1,8 @@
+class Zone:
+    def __init(self, name, x, y, zone="normal", max_drones=1):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.zone = zone
+        self.max_drones = max_drones
+        
